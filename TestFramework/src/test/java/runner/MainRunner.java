a1894bbd6c0ece26b88dc342e632163cee5a92bd
Israel Mesa <mesa.israel50@gmail.com>
2020-02-17 15:46:41 -0500

    use testNg as test runner

--- TestFramework/src/test/java/runner/MainRunner.java
+++ TestFramework/src/test/java/runner/MainRunner.java
@@ -1,10 +1,8 @@
 package runner;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/java/featureFiles/Login.feature"},
         glue = {"steps"},
@@ -13,5 +11,5 @@ import org.junit.runner.RunWith;
         plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json", "com.cucumber.listener.ExtentCucumberFormatter:target/report.html"}
 )
 
-public class MainRunner {
+public class MainRunner extends AbstractTestNGCucumberTests {
 }
