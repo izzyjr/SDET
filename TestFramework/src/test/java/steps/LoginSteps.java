de942a21ac5b5cdec3fb18d979bea296a442222b
Israel Mesa <mesa.israel50@gmail.com>
2020-02-17 15:46:01 -0500

    close driver

--- TestFramework/src/test/java/steps/LoginSteps.java
+++ TestFramework/src/test/java/steps/LoginSteps.java
@@ -36,12 +36,12 @@ public class LoginSteps {
 
     @Given("^user enters a valid username$")
     public void user_enters_a_valid_username() throws Throwable {
-        driver.findElement(By.xpath("//*[@id='email']")).sendKeys(" ");
+        driver.findElement(By.xpath("//*[@id='email']")).sendKeys("");
     }
 
     @Given("^user enters a valid password$")
     public void user_enters_a_valid_password() throws Throwable {
-        driver.findElement(By.xpath("//*[@id='password']")).sendKeys(" ");
+        driver.findElement(By.xpath("//*[@id='password']")).sendKeys("");
     }
 
     @When("^user clicks on the login button$")
@@ -54,5 +54,6 @@ public class LoginSteps {
         Thread.sleep(3000);
         WebElement askQuestionButton = driver.findElement(By.xpath("//a[contains(text(), 'Ask Question')]"));
         Assert.assertTrue(askQuestionButton.isDisplayed());
+        driver.close();
     }
 }
