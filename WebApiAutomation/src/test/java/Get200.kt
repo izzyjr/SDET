c724051fd65d877f636c1d042003159c7ca7707f
Israel Mesa <mesa.israel50@gmail.com>
2020-05-24 19:04:40 -0400

    Simple 200 api check test

---
+++ WebApiAutomation/src/test/java/Get200.kt
@@ -0,0 +1,24 @@
+import org.apache.http.HttpResponse
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.CloseableHttpClient
+import org.apache.http.impl.client.HttpClientBuilder
+import org.testng.Assert
+import org.testng.annotations.Test
+
+class Get200 {
+
+    private val client: CloseableHttpClient = HttpClientBuilder.create().build()
+
+    @Test
+    fun baseUrlReturns200() {
+        val get = HttpGet(BASE_URL)
+        val response: HttpResponse = client.execute(get)
+        val actualStatus = response.statusLine.statusCode
+        Assert.assertEquals(actualStatus, 200)
+    }
+
+    companion object {
+      const val BASE_URL = "https://api.github.com"
+    }
+
+}
