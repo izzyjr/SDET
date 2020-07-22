af337c8e26204b2efabc0e8e81352669147ea40a
Israel Mesa <mesa.israel50@gmail.com>
2020-07-21 23:11:44 -0400

    demoTest - in progress

---
+++ WebDriverAutomation/src/main/java/com/mesa/MesaUtils.kt
@@ -0,0 +1,17 @@
+package com.mesa
+
+open class MesaUtils {
+
+    companion object {
+
+        const val PATH = "/Users/israelmesa/Desktop/ChromeDriver/chromedriver"
+        const val GOOGLE_SEARCH_BAR = "//*[@class=\"gLFyf gsfi\"]"
+        // brittle xpath
+        const val GOOGLE_SEARCH_BUTTON = "//*[@id=\"tsf\"]/div[2]/div[1]/div[3]/center/input[1]"
+        const val MOVIE = "The Fifth Element"
+        const val SEARCH = "pluralsight"
+        const val IMAGE = "//*[@data-iml=\"714.9199999985285\"]"
+
+    }
+
+}
---
+++ WebDriverAutomation/src/test/java/com/mesa/WebDriverDemo.kt
@@ -0,0 +1,45 @@
+package com.mesa
+
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.support.ui.WebDriverWait
+import org.testng.annotations.BeforeMethod
+import org.testng.annotations.Test
+import java.util.concurrent.TimeUnit
+
+class WebDriverDemo {
+
+    private lateinit var driver: WebDriver
+    private lateinit var wait: WebDriverWait
+
+    @BeforeMethod
+    fun startUpBrowser() {
+        System.setProperty("webdriver.chrome.driver", MesaUtils.PATH)
+        driver = ChromeDriver()
+        driver.get("https://google.com")
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+        driver.manage().window().fullscreen()
+    }
+
+//    @Test
+//    fun searchMovie() {
+//        val searchField: WebElement = driver.findElement(By.xpath(MesaUtils.GOOGLE_SEARCH_BAR))
+//        searchField.sendKeys(MesaUtils.MOVIE)
+//        val searchButton: WebElement = driver.findElement(By.xpath(MesaUtils.GOOGLE_SEARCH_BUTTON))
+//        searchButton.click()
+//    }
+
+    @Test
+    fun demoTest() {
+        val searchField: WebElement = driver.findElement(By.xpath(MesaUtils.GOOGLE_SEARCH_BAR))
+        searchField.sendKeys(MesaUtils.SEARCH)
+        searchField.submit()
+        val imagesLink: WebElement = driver.findElements(By.linkText("Images"))[0]
+        imagesLink.click()
+        val image: WebElement = driver.findElement(By.xpath(MesaUtils.IMAGE))
+        image.click()
+    }
+
+}
