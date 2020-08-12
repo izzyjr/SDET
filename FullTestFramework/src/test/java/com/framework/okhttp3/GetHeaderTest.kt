b949ddb707beae493e52ec432586e971773f25c0
Israel Mesa <mesa.israel50@gmail.com>
2020-08-11 21:27:31 -0400

    Testing Headers

--- FullTestFramework/src/test/java/com/framework/okhttp3/GetHeaderTest.kt
+++ FullTestFramework/src/test/java/com/framework/okhttp3/GetHeaderTest.kt
@@ -5,6 +5,7 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import org.testng.AssertJUnit.assertEquals
+import org.testng.annotations.DataProvider
 import org.testng.annotations.Test
 
 class GetHeaderTest {
@@ -23,6 +24,36 @@ class GetHeaderTest {
 
         // Assert
         assertEquals(200, actualCode)
+
+    }
+
+    @Test
+    fun contentTypeIsJson() {
+
+        // Arrange
+        val httpClient: OkHttpClient = OkHttpClient()
+        val request: Request = Request.Builder().addHeader("User-Agent", "OkHttp3")
+                .url(BASE_URL).get().build()
+
+        // Act
+        val response: Response = httpClient.newCall(request).execute()
+        val actualContentType: String = response.headers["content-type"]!!
+        assertEquals("application/json; charset=utf-8", actualContentType)
+
+    }
+
+    @Test
+    fun xRateLimitIsPresent() {
+
+        // Arrange
+        val httpClient: OkHttpClient = OkHttpClient()
+        val request: Request = Request.Builder().addHeader("User-Agent", "OkHttp3")
+                .url(BASE_URL).get().build()
+
+        // Act
+        val response: Response = httpClient.newCall(request).execute()
+        val xRateLimit: String = response.headers["X-RateLimit-Limit"]!!
+        assertEquals(60, xRateLimit.toInt())
 
     }
 
