73850c6aad32884c28dd10da768a07edb5c1ac52
Israel Mesa <mesa.israel50@gmail.com>
2020-08-13 22:30:59 -0400

    Delete

--- FullTestFramework/src/test/java/com/framework/okhttp3/PostAndDelete.kt
+++ FullTestFramework/src/test/java/com/framework/okhttp3/PostAndDelete.kt
@@ -2,19 +2,20 @@ package com.framework.okhttp3
 
 import Utils.Companion.EMAIL
 import Utils.Companion.PASSWORD
+import Utils.Companion.TOKEN
 import com.framework.MesaUtils.Companion.BASE_URL
 import okhttp3.*
 import org.testng.Assert.assertEquals
 import org.testng.annotations.Test
 import okhttp3.RequestBody.Companion.toRequestBody
 
-class PostTest {
+class PostAndDelete {
+
+    private val httpClient: OkHttpClient = OkHttpClient()
+    private lateinit var response: Response
 
     @Test
     fun postWithoutAuthorizationFails() {
-
-        // Arrange - create client
-        val httpClient: OkHttpClient = OkHttpClient()
 
         // Arrange - create request
         val json: String = "{\"name\": \"deleteme\"}"
@@ -26,12 +27,27 @@ class PostTest {
                 .build()
 
         // Act - send request
-        val response: Response = httpClient.newCall(request).execute()
+        response = httpClient.newCall(request).execute()
         val actualCode: Int = response.code
 
         // Assert
         assertEquals(actualCode, 201)
+    }
 
+    @Test
+    fun deleteIsSuccessful() {
+
+        // Arrange - create request
+        val request: Request = Request.Builder()
+                .url("$BASE_URL/repos/izzyjr/deleteme")
+                .addHeader("Authorization", "token $TOKEN")
+                .delete()
+                .build()
+        response = httpClient.newCall(request).execute()
+        val actualCode: Int = response.code
+
+        // Assert
+        assertEquals(actualCode, 204)
     }
 
 }
