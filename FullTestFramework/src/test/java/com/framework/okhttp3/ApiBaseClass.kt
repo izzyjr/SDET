62968372d397cad6b5022689db0d18fac6006396
Israel Mesa <mesa.israel50@gmail.com>
2020-08-15 20:12:44 -0400

    refactor

--- FullTestFramework/src/test/java/com/framework/okhttp3/ApiBaseClass.kt
+++ FullTestFramework/src/test/java/com/framework/okhttp3/ApiBaseClass.kt
@@ -11,7 +11,7 @@ open class ApiBaseClass {
     protected val httpClient: OkHttpClient = OkHttpClient()
     protected lateinit var response: Response
     protected lateinit var responseBody: ResponseBody
-    protected lateinit var jsonBody: String
+    private lateinit var jsonBody: String
 
     open fun userSendRequest(): JSONObject {
         // Arrange - create request
@@ -33,16 +33,12 @@ open class ApiBaseClass {
 
     companion object {
         const val BASE_URL: String = "https://api.github.com"
+
         private val case1: Array<String> = arrayOf("content-type", "application/json; charset=utf-8")
         private val case2: Array<String> = arrayOf("X-RateLimit-Limit", "60")
         private val case3: Array<String> = arrayOf("server", "GitHub.com")
         private val case4: Array<String> = arrayOf("X-frame-options", "deny")
         val testHeaders: ArrayList<Array<String>> = arrayListOf(case1, case2, case3, case4)
-
-        const val LOGIN = "login"
-        const val ID = "id"
-        const val TYPE = "type"
-        const val EMAIL = "email"
     }
 
 }
--- WebDriverAutomation/src/main/java/com/mesa/MesaUtils.kt
+++ WebDriverAutomation/src/main/java/com/mesa/MesaUtils.kt
@@ -4,7 +4,8 @@ open class MesaUtils {
 
     companion object {
 
-        const val PATH = "/Users/israelmesa/Desktop/ChromeDriver/chromedriver"
+        const val CHROME = "/Users/israelmesa/Desktop/ChromeDriver/chromedriver"
+        const val GECKO = "/Users/israelmesa/Desktop/GeckoDriver/geckodriver"
         const val GOOGLE_SEARCH_BAR = "//*[@class=\"gLFyf gsfi\"]"
         // brittle xpath
         const val GOOGLE_SEARCH_BUTTON = "//*[@id=\"tsf\"]/div[2]/div[1]/div[3]/center/input[1]"
